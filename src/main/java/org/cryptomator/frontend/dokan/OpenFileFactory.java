402b2d82f9c709a7aaf0aa8c634ce3fa6ded560f
infeo <armin.schrenk@zoho.eu>
2018-04-19 11:28:02 +0200

    prevent a fileHandle-value of zero

--- src/main/java/org/cryptomator/frontend/dokan/OpenFileFactory.java
+++ src/main/java/org/cryptomator/frontend/dokan/OpenFileFactory.java
@@ -21,7 +21,7 @@ public class OpenFileFactory implements AutoCloseable {
 	private static final Logger LOG = LoggerFactory.getLogger(OpenFileFactory.class);
 
 	private final ConcurrentMap<Long, OpenFile> openFiles = new ConcurrentHashMap<>();
-	private final AtomicLong fileHandleGen = new AtomicLong();
+	private final AtomicLong fileHandleGen = new AtomicLong(1);
 
 	public OpenFileFactory() {
 	}
@@ -32,12 +32,13 @@ public class OpenFileFactory implements AutoCloseable {
 	 * @return file handle used to identify and close open files.
 	 * @throws IOException
 	 */
-	public long open(Path path,  OpenOption... options) throws IOException {
-		return this.open(path,  Sets.newHashSet(options));
+	public long open(Path path, OpenOption... options) throws IOException {
+		return this.open(path, Sets.newHashSet(options));
 	}
 
 
 	/**
+	 * Creates an Openfile with the given path  and options & attributes and assigns a fileHandle != 0 to it
 	 *
 	 * @param path path of the file to open
 	 * @param options file open options
@@ -47,6 +48,9 @@ public class OpenFileFactory implements AutoCloseable {
 	 */
 	public long open(Path path, Set<? extends OpenOption> options, FileAttribute<?>... attrs) throws IOException {
 		long fileHandle = fileHandleGen.getAndIncrement();
+		if (fileHandle == 0) {
+			fileHandle = fileHandleGen.getAndIncrement();
+		}
 		openFiles.put(fileHandle, new OpenFile(path, options, attrs));
 		return fileHandle;
 	}
@@ -57,7 +61,7 @@ public class OpenFileFactory implements AutoCloseable {
 
 	/**
 	 * Closes the channel identified by the given fileHandle
-	 * 
+	 *
 	 * @param fileHandle file handle used to identify
 	 * @throws ClosedChannelException If no channel for the given fileHandle has been found.
 	 * @throws IOException
@@ -74,13 +78,13 @@ public class OpenFileFactory implements AutoCloseable {
 	/**
 	 * Closes all currently open files.
 	 * Calling this method will not prevent the factory to open new files, i.e. this method can be called multiple times and is not idempotent.
-	 * 
+	 *
 	 * @throws IOException
 	 */
 	@Override
 	public synchronized void close() throws IOException {
 		IOException exception = new IOException("At least one open file could not be closed.");
-		for (Iterator<Map.Entry<Long, OpenFile>> it = openFiles.entrySet().iterator(); it.hasNext();) {
+		for (Iterator<Map.Entry<Long, OpenFile>> it = openFiles.entrySet().iterator(); it.hasNext(); ) {
 			Map.Entry<Long, OpenFile> entry = it.next();
 			OpenFile openFile = entry.getValue();
 			LOG.warn("Closing unclosed file {}", openFile);
