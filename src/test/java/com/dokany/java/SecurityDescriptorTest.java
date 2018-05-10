6ace40e52a170c59d9f6ad4afc3b31cf491fda43
infeo <armin.schrenk@zoho.eu>
2018-05-10 12:02:19 +0200

    fixing junit-dependency and adding testclass stub for the security descriptor

---
+++ src/test/java/com/dokany/java/SecurityDescriptorTest.java
@@ -0,0 +1,17 @@
+package com.dokany.java;
+
+import org.junit.Test;
+
+public class SecurityDescriptorTest {
+
+	@Test
+	public void testEmptySecurityDescriptor(){
+
+	}
+
+
+	public static byte [] getEmptySecurityDescriptor(){
+
+	}
+
+}
