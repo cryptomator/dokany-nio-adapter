58a448bf8bd0aac8a5b4d2238124914c0ecf5b87
infeo <armin.schrenk@zoho.eu>
2018-05-24 14:34:32 +0200

    reversing the mask of the SD control enum to correct ordering

--- src/main/java/com/dokany/java/structure/filesecurity/SecurityDescriptorControlFlag.java
+++ src/main/java/com/dokany/java/structure/filesecurity/SecurityDescriptorControlFlag.java
@@ -12,97 +12,97 @@ public enum SecurityDescriptorControlFlag implements EnumInteger {
 	 * Self-Relative
 	 * Set when the security descriptor is in self-relative format. Cleared when the security descriptor is in absolute format.
 	 */
-	SR(1 << 0),
+	SR(1 << 15),
 
 	/**
 	 * RM Control Valid
 	 * Set to 0x1 when the Sbz1 field is to be interpreted as resource manager control bits.
 	 */
-	RM(1 << 1),
+	RM(1 << 14),
 
 	/**
 	 * SACL Protected
 	 * Set when the SACL will be protected from inherit operations.
 	 */
-	PS(1 << 2),
+	PS(1 << 13),
 
 	/**
 	 * DACL Protected
 	 * Set when the DACL will be protected from inherit operations.
 	 */
-	PD(1 << 3),
+	PD(1 << 12),
 
 	/**
 	 * SACL Auto-Inherited
 	 * Set when the SACL was created through inheritance.
 	 */
-	SI(1 << 4),
+	SI(1 << 11),
 
 	/**
 	 * DACL Auto-Inherited
 	 * Set when the DACL was created through inheritance.
 	 */
-	DI(1 << 5),
+	DI(1 << 10),
 
 	/**
 	 * SACL Computed Inheritance Required
 	 * Set when the SACL is to be computed through inheritance. When both SC and SI are set, the resulting security descriptor sets SI; the SC setting is not preserved.
 	 */
-	SC(1 << 6),
+	SC(1 << 9),
 
 	/**
 	 * DACL Computed Inheritance Required
 	 * Set when the DACL is to be computed through inheritance. When both DC and DI are set, the resulting security descriptor sets DI; the DC setting is not preserved.
 	 */
-	DC(1 << 7),
+	DC(1 << 8),
 
 	/**
 	 * Server Security
 	 * Set when the caller wants the system to create a Server ACL based on the input ACL, regardless of its source (explicit or defaulting).
 	 */
-	SS(1 << 8),
+	SS(1 << 7),
 
 	/**
 	 * DACL Trusted
 	 * Set when the ACL that is pointed to by the DACL field was provided by a trusted source and does not require any editing of compound ACEs.
 	 */
-	DT(1 << 9),
+	DT(1 << 6),
 
 	/**
 	 * SACL Defaulted
 	 * Set when the SACL was established by default means.
 	 */
-	SD(1 << 10),
+	SD(1 << 5),
 
 	/**
 	 * SACL Present
 	 * Set when the SACL is present on the object.
 	 */
-	SP(1 << 11),
+	SP(1 << 4),
 
 	/**
 	 * DACL Defaulted
 	 * Set when the DACL was established by default means.
 	 */
-	DD(1 << 12),
+	DD(1 << 3),
 
 	/**
 	 * DACL Present
 	 * Set when the DACL is present on the object.
 	 */
-	DP(1 << 13),
+	DP(1 << 2),
 
 	/**
 	 * Group Defaulted
 	 * Set when the group was established by default means.
 	 */
-	GD(1 << 14),
+	GD(1 << 1),
 
 	/**
 	 * Owner Defaulted
 	 * Set when the owner was established by default means.
 	 */
-	OD(1 << 15);
+	OD(1 << 0);
 
 	private final int mask;
 
