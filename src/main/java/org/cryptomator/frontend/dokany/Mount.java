5fbe28e08562033809fcdf6ff4b2a25e8952436b
Sebastian Stenzel <sebastian.stenzel@skymatic.de>
2018-06-15 15:02:40 +0200

    using new mount method in RW test

--- src/main/java/org/cryptomator/frontend/dokany/Mount.java
+++ src/main/java/org/cryptomator/frontend/dokany/Mount.java
@@ -2,17 +2,26 @@ package org.cryptomator.frontend.dokany;
 
 import com.dokany.java.DokanyDriver;
 
+import java.util.concurrent.CompletableFuture;
+
 public class Mount implements AutoCloseable {
 
     private final DokanyDriver driver;
+    private final  CompletableFuture<Void> driverJob;
 
     public Mount(DokanyDriver driver) {
         this.driver = driver;
-        driver.start();
+		this.driverJob = CompletableFuture.runAsync(driver::start);
     }
 
     @Override
     public void close() {
-        driver.shutdown();
+        try {
+            driver.shutdown();
+        } catch (Exception e) {
+        	e.printStackTrace();
+		}
+		// TODO: force quit?
+		// driverJob.cancel(true);
     }
 }
