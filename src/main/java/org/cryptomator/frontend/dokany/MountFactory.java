28ca27c4a611d2032f9b42caab0ef6a8f14a48a1
infeo <armin.schrenk@zoho.eu>
2018-06-28 15:58:20 +0200

    reformatting

--- src/main/java/org/cryptomator/frontend/dokany/MountFactory.java
+++ src/main/java/org/cryptomator/frontend/dokany/MountFactory.java
@@ -23,47 +23,48 @@ public class MountFactory {
 
 	private static final Logger LOG = LoggerFactory.getLogger(MountFactory.class);
 	private static final int MOUNT_TIMEOUT_MS = 5000;
-    private static final short THREAD_COUNT = 1;
-    private static final EnumIntegerSet<MountOption> MOUNT_OPTIONS = new EnumIntegerSet<>( //
-    		// MountOption.DEBUG_MODE, //
-            // MountOption.STD_ERR_OUTPUT, //
-            MountOption.REMOVABLE_DRIVE, //
-            MountOption.CURRENT_SESSION);
-    private static final EnumIntegerSet<FileSystemFeature> FILE_SYSTEM_FEATURES = new EnumIntegerSet<>( //
-    		FileSystemFeature.CASE_PRESERVED_NAMES, //
-            FileSystemFeature.CASE_SENSITIVE_SEARCH, //
-            // FileSystemFeature.PERSISTENT_ACLS, //
-            // FileSystemFeature.SUPPORTS_REMOTE_STORAGE, //
-            FileSystemFeature.UNICODE_ON_DISK);
-    private static final String UNC_NAME = "";
-    private static final int TIMEOUT = 10000;
-    private static final int ALLOC_UNIT_SIZE = 4096;
-    private static final int SECTOR_SIZE = 4096;
+	private static final short THREAD_COUNT = 1;
+	private static final EnumIntegerSet<MountOption> MOUNT_OPTIONS = new EnumIntegerSet<>( //
+			// MountOption.DEBUG_MODE, //
+			// MountOption.STD_ERR_OUTPUT, //
+			MountOption.REMOVABLE_DRIVE, //
+			MountOption.CURRENT_SESSION);
+	private static final EnumIntegerSet<FileSystemFeature> FILE_SYSTEM_FEATURES = new EnumIntegerSet<>( //
+			FileSystemFeature.CASE_PRESERVED_NAMES, //
+			FileSystemFeature.CASE_SENSITIVE_SEARCH, //
+			// FileSystemFeature.PERSISTENT_ACLS, //
+			// FileSystemFeature.SUPPORTS_REMOTE_STORAGE, //
+			FileSystemFeature.UNICODE_ON_DISK);
+	private static final String UNC_NAME = "";
+	private static final int TIMEOUT = 10000;
+	private static final int ALLOC_UNIT_SIZE = 4096;
+	private static final int SECTOR_SIZE = 4096;
 
-    private final ExecutorService executorService;
+	private final ExecutorService executorService;
 
-    public MountFactory(ExecutorService executorService) {
-        this.executorService = executorService;
-    }
+	public MountFactory(ExecutorService executorService) {
+		this.executorService = executorService;
+	}
 
 	/**
 	 * Mounts a drive with the given drive letter containing contents of the given path.
 	 * This method blocks until the mount succeeds or times out.
+	 *
 	 * @param fileSystemRoot Path to the directory which will be the content root of the mounted drive.
 	 * @param driveLetter The drive letter of the mounted drive.
 	 * @param volumeName The name of the drive as shown to the user.
 	 * @param fileSystemName The technical file system name shown in the drive properties window.
 	 * @return The mount object, regardless if mount succeeded.
 	 */
-    public Mount mount(Path fileSystemRoot, char driveLetter, String volumeName, String fileSystemName) {
-        String mountPoint = driveLetter + ":\\";
-        DeviceOptions deviceOptions = new DeviceOptions(mountPoint, THREAD_COUNT, MOUNT_OPTIONS, UNC_NAME, TIMEOUT, ALLOC_UNIT_SIZE, SECTOR_SIZE);
-        VolumeInformation volumeInfo = new VolumeInformation(VolumeInformation.DEFAULT_MAX_COMPONENT_LENGTH, volumeName, 0x98765432, fileSystemName, FILE_SYSTEM_FEATURES);
+	public Mount mount(Path fileSystemRoot, char driveLetter, String volumeName, String fileSystemName) {
+		String mountPoint = driveLetter + ":\\";
+		DeviceOptions deviceOptions = new DeviceOptions(mountPoint, THREAD_COUNT, MOUNT_OPTIONS, UNC_NAME, TIMEOUT, ALLOC_UNIT_SIZE, SECTOR_SIZE);
+		VolumeInformation volumeInfo = new VolumeInformation(VolumeInformation.DEFAULT_MAX_COMPONENT_LENGTH, volumeName, 0x98765432, fileSystemName, FILE_SYSTEM_FEATURES);
 		CompletableFuture<Void> mountDidSucceed = new CompletableFuture<>();
-        DokanyFileSystem dokanyFs = new ReadWriteAdapter(fileSystemRoot, volumeInfo, mountDidSucceed);
-        DokanyDriver dokanyDriver = new DokanyDriver(deviceOptions, dokanyFs);
+		DokanyFileSystem dokanyFs = new ReadWriteAdapter(fileSystemRoot, volumeInfo, mountDidSucceed);
+		DokanyDriver dokanyDriver = new DokanyDriver(deviceOptions, dokanyFs);
 		LOG.debug("Mounting on drive {}: ...", driveLetter);
-        Mount mount = new Mount(executorService, driveLetter, dokanyDriver);
+		Mount mount = new Mount(executorService, driveLetter, dokanyDriver);
 		try {
 			mountDidSucceed.get(MOUNT_TIMEOUT_MS, TimeUnit.MILLISECONDS);
 			LOG.debug("Mounted drive {}: successfully.", driveLetter);
@@ -74,16 +75,16 @@ public class MountFactory {
 		} catch (TimeoutException e) {
 			LOG.warn("Mounting timed out.");
 		} finally {
-			if(driveLetter == 65) {
+			if (driveLetter == 65) {
 				LOG.error("Dokany does not support Drive letter \'A\' as mount point.");
 			}
 			return mount;
 		}
-    }
+	}
 
-    public static boolean isApplicable() {
-        return System.getProperty("os.name").toLowerCase().contains("windows")
-                && Files.exists(Paths.get("C:\\Windows\\System32\\drivers\\dokan1.sys")); // https://github.com/dokan-dev/dokany/wiki/How-to-package-your-application-with-Dokan#check-for-previous-dokan-installations
-    }
+	public static boolean isApplicable() {
+		return System.getProperty("os.name").toLowerCase().contains("windows")
+				&& Files.exists(Paths.get("C:\\Windows\\System32\\drivers\\dokan1.sys")); // https://github.com/dokan-dev/dokany/wiki/How-to-package-your-application-with-Dokan#check-for-previous-dokan-installations
+	}
 
 }
