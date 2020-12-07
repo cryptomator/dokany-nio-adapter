359745d20c6df2c834d95a4011fdb33ed3b0b516
Armin Schrenk <armin.schrenk@skymatic.de>
2020-12-07 17:56:16 +0100

    Refactor DokanDriver class: * save future result of (blocking) dokan process * if process exits outside of JVM, set mount state accordingly and log on error * mount result is checked in completionstage chain * start/mount method throws Dokany exception

--- src/main/java/com/dokany/java/DokanyDriver.java
+++ src/main/java/com/dokany/java/DokanyDriver.java
@@ -9,7 +9,6 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * Main class to start and stop Dokany file system.
@@ -19,13 +18,15 @@ public final class DokanyDriver {
 	private static final Logger LOG = LoggerFactory.getLogger(DokanyDriver.class);
 	private final DeviceOptions deviceOptions;
 	private final DokanyFileSystem fileSystem;
-	private final AtomicBoolean isMounted;
+
+	private volatile boolean isMounted;
+	private volatile CompletableFuture mountFuture;
 
 	public DokanyDriver(final DeviceOptions deviceOptions, final DokanyFileSystem fileSystem) {
-
 		this.deviceOptions = deviceOptions;
 		this.fileSystem = fileSystem;
-		this.isMounted = new AtomicBoolean(false);
+		this.mountFuture = CompletableFuture.failedFuture(new IllegalStateException("Not mounted."));
+		this.isMounted = false;
 
 		LOG.info("Dokany version: {}", getVersion());
 		LOG.info("Dokany driver version: {}", getDriverVersion());
@@ -64,66 +65,72 @@ public final class DokanyDriver {
 	/**
 	 * Calls {@link NativeMethods#DokanMain(DeviceOptions, DokanyOperations)}. Has {@link java.lang.Runtime#addShutdownHook(Thread)} which calls {@link #shutdown()}
 	 */
-	public synchronized void start() {
-		if (!isMounted.get()) {
+	public synchronized void start() throws DokanyException {
+		if (!isMounted) {
 			try {
-				Runtime.getRuntime().addShutdownHook(new Thread() {
-					@Override
-					public void run() {
-						shutdown();
-					}
-				});
+				Runtime.getRuntime().addShutdownHook(new Thread(this::shutdown));
 
-				int mountStatus = 0;
+				//real mount op
+				mountFuture = CompletableFuture
+						.supplyAsync(() -> NativeMethods.DokanMain(deviceOptions, new DokanyOperationsProxy(fileSystem)))
+						.handle((returnVal, exception) -> {
+							setIsMounted(false);
+							if (returnVal != null && returnVal.equals(0)) {
+								return 0;
+							}
 
+							if (returnVal == null) {
+								throw new DokanyException(exception);
+							} else {
+								throw new DokanyException("Return Code " + returnVal + ": " + MountError.fromInt(returnVal).getDescription());
+							}
+						});
 
+				//check return value
 				try {
-					mountStatus = CompletableFuture
-							.supplyAsync(() -> NativeMethods.DokanMain(deviceOptions, new DokanyOperationsProxy(fileSystem)))
-							.get(2900, TimeUnit.MILLISECONDS);
+					mountFuture.get(1000, TimeUnit.MILLISECONDS);
 				} catch (TimeoutException e) {
-					// ok
-					mountStatus = 0;
+					// up and running
 				} catch (ExecutionException e) {
-					mountStatus = -1;
+					LOG.error("Error while mounting", e);
+					if (e.getCause() instanceof DokanyException) {
+						throw (DokanyException) e.getCause();
+					} else {
+						throw new DokanyException(e);
+					}
 				} catch (InterruptedException e) {
 					Thread.currentThread().interrupt();
 				}
 
-
-				if (mountStatus < 0) {
-					throw new IllegalStateException(MountError.fromInt(mountStatus).getDescription());
-				} else {
-					isMounted.set(true);
-				}
-
-
+				setIsMounted(true);
 			} catch (UnsatisfiedLinkError err) {
 				LOG.error("Unable to load dokan driver.", err);
 				throw new LibraryNotFoundException(err.getMessage());
-			} catch (Throwable e) {
-				LOG.warn("Error while mounting", e);
-				throw e;
 			}
 		} else {
-			LOG.info("Dokan Device already mounted on {}.", deviceOptions.MountPoint);
+			LOG.debug("Dokan Device already mounted on {}.", deviceOptions.MountPoint);
 		}
 	}
+
+	public synchronized void setIsMounted(boolean newValue) {
+		isMounted = newValue;
+	}
+
 
 	/**
 	 * Unmounts the Dokan Device from the mount point given in the mount options.
 	 */
 	public synchronized void shutdown() {
 		LOG.info("Unmounting Dokan device at {}", deviceOptions.MountPoint);
-		if (isMounted.get()) {
+		if (isMounted) {
 			boolean unmounted = NativeMethods.DokanRemoveMountPoint(deviceOptions.MountPoint);
 			if (unmounted) {
-				isMounted.set(false);
+				setIsMounted(false);
 			} else {
 				LOG.error("Unable to unmount Dokan device at {}.", deviceOptions.MountPoint);
 			}
 		} else {
-			LOG.info("Dokan Device not mounted at {}.", deviceOptions.MountPoint);
+			LOG.debug("Dokan Device not mounted at {}.", deviceOptions.MountPoint);
 		}
 	}
 
