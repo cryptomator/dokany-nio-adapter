60c97cdcbeb606280d6d7869e8891408a6c1b5a5
Armin Schrenk <armin.schrenk@skymatic.de>
2021-01-15 12:19:46 +0100

    Added constructor to revealException to contain message and cause

--- src/main/java/org/cryptomator/frontend/dokany/RevealException.java
+++ src/main/java/org/cryptomator/frontend/dokany/RevealException.java
@@ -2,11 +2,15 @@ package org.cryptomator.frontend.dokany;
 
 public class RevealException extends Exception {
 
-	public RevealException(String msg){
+	public RevealException(String msg) {
 		super(msg);
 	}
 
-	public RevealException(Throwable cause){
+	public RevealException(Throwable cause) {
 		super(cause);
+	}
+
+	public RevealException(String msg, Throwable cause) {
+		super(msg, cause);
 	}
 }
