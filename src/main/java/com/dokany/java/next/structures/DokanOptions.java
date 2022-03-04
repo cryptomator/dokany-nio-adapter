cryptomator/dokany-nio-adapter

commit 0b0d04a4c4a0ed3d092c7d6049309b31d928a3e7

prvent JNA auto writing to mem for DokanOptions

src/main/java/com/dokany/java/next/structures/DokanOptions.java
@@ -41,15 +41,14 @@ public class DokanOptions extends Structure implements Structure.ByReference {
 	 * Mount point. It can be a driver letter like "M:\" or a folder path "C:\mount\dokan" on a NTFS partition.
 	 */
 	//LPCWSTR
-	//TODO: should be volatile?
-	public WString MountPoint;
+	public volatile WString MountPoint;
 
 	/**
 	 * UNC Name for the Network Redirector
 	 * \see <a href="https://msdn.microsoft.com/en-us/library/windows/hardware/ff556761(v=vs.85).aspx">Support for UNC Naming</a>
 	 */
 	//LPCWSTR
-	public WString UNCName;
+	public volatile WString UNCName;
 
 	/**
 	 * Max timeout in milliseconds of each request before Dokan gives up to wait events to complete.
@@ -81,7 +80,7 @@ public class DokanOptions extends Structure implements Structure.ByReference {
 	/**
 	 * Optional Volume Security descriptor. See <a href="https://docs.microsoft.com/en-us/windows/win32/api/securitybaseapi/nf-securitybaseapi-initializesecuritydescriptor">InitializeSecurityDescriptor</a>
 	 */
-	public byte[] VolumeSecurityDescriptor = new byte[VOLUME_SECURITY_DESCRIPTOR_MAX_SIZE];
+	public volatile byte[] VolumeSecurityDescriptor = new byte[VOLUME_SECURITY_DESCRIPTOR_MAX_SIZE];
 
 	public int getVersion() {
 		return Short.toUnsignedInt(Version);

commit c94c2bf49f62f4b07548b0589c16d0f49e2b3ff7

fixing bug introduced in 0b0d04a4c4a0ed3d092c7d6049309b31d928a3e7

src/main/java/com/dokany/java/next/structures/DokanOptions.java
@@ -193,8 +193,8 @@ public class DokanOptions extends Structure implements Structure.ByReference {
 			dokanOptions.writeField("SingleThread", singleThread);
 			dokanOptions.writeField("Options", options);
 			dokanOptions.writeField("GlobalContext", globalContext);
-			dokanOptions.MountPoint = new WString(mountPoint);
-			dokanOptions.UNCName = new WString(uncName);
+			dokanOptions.writeField("MountPoint", new WString(mountPoint));
+			dokanOptions.writeField("UNCName", new WString(uncName));
 			dokanOptions.writeField("Timeout", timeout);
 			dokanOptions.writeField("AllocationUnitSize", allocationUnitSize);
 			dokanOptions.writeField("SectorSize", sectorSize);