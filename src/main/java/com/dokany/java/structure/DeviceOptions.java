4a95d2f22095c8a93c487e27bceabfafd0ad8a49
Armin Schrenk <armin.schrenk@skymatic.de>
2022-11-08 10:18:13 +0100

    bump requested dokany features

--- src/main/java/com/dokany/java/structure/DeviceOptions.java
+++ src/main/java/com/dokany/java/structure/DeviceOptions.java
@@ -13,9 +13,9 @@ import com.sun.jna.WString;
  */
 public class DeviceOptions extends Structure implements Structure.ByReference {
 	/**
-	 * Version of the Dokany features requested (version "123" is equal to Dokany version 1.2.3). Currently is 100.
+	 * Version of the Dokany features requested (version "123" is equal to Dokany version 1.2.3).
 	 */
-	public short Version = 110;
+	public short Version = 150;
 	/**
 	 * Number of threads to be used internally by Dokany library. More thread will handle more event at the same time.
 	 */
