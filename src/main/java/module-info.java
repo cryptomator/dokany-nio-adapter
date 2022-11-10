cb2c06563b3c871c8c95a20d1e5a975818e24b5c
Armin Schrenk <armin.schrenk@skymatic.de>
2022-11-10 15:28:02 +0100

    remove unnecessary open in module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,7 +8,6 @@ module org.cryptomator.frontend.dokany {
 	requires commons.cli;
 	requires com.google.common;// TODO try to remove
 
-	opens org.cryptomator.frontend.dokany.internal to com.sun.jna;
 	opens org.cryptomator.frontend.dokany.internal.structure to com.sun.jna;
 	opens org.cryptomator.frontend.dokany.internal.structure.filesecurity to com.sun.jna;
 
