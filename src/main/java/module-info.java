952994c3b9ea105a2a82c35ab3a84286de5e87e9
Armin Schrenk <armin.schrenk@skymatic.de>
2022-11-10 11:18:45 +0100

    add module.info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+import org.cryptomator.frontend.dokany.DokanyMountProvider;
+
+module org.cryptomator.frontend.dokany {
+	requires org.cryptomator.integrations.api;
+	requires org.slf4j;
+	requires com.sun.jna.platform;
+	requires com.sun.jna;
+	requires commons.cli;
+	requires com.google.common;// TODO try to remove
+
+	opens com.dokany.java to com.sun.jna;
+	opens com.dokany.java.structure to com.sun.jna;
+	opens com.dokany.java.structure.filesecurity to com.sun.jna;
+
+	provides org.cryptomator.integrations.mount.MountService with DokanyMountProvider;
+}
